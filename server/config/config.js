3181dc9eb6b4f95a5b7545e854e6c0072add3607
Claudio Melendres <claudiomelendres@gmail.com>
2019-01-01 16:35:40 -0400

    changes in server.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,22 @@
+// ====================================================
 // Puerto
+// ====================================================
 process.env.PORT = process.env.PORT || 3000;
+
+// ====================================================
+// Entorrno
+// ====================================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// ====================================================
+// Base de Datos
+// ====================================================
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:Informatica3210.@ds147354.mlab.com:47354/cafe';
+}
+
+process.env.URLDB = urlDB;
